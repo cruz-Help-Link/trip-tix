1508ddab368a11f54bc2716bdae2f99448066427
Eniola Fanegan <eniolachinemerem74@gmail.com>
2024-06-20 01:41:46 -0500

    Update LocationController.java

--- triptix -update/src/main/java/com/example/triptix/controller/LocationController.java
+++ triptix -update/src/main/java/com/example/triptix/controller/LocationController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/locations")
